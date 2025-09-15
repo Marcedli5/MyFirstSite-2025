134355cd1952c5dcfadc077c73aee3cc440396fb
Marcedli5 <nagyagi.marcell@gmail.com>
2025-09-15 13:24:00 +0200

    asd

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/asd', function () {
+Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return "hello world";
+});
+
+Route::get('/valami', function () {
+    return view('valami');
 });
